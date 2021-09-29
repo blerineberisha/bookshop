dc1dd53196e0c567bbf93c17a4799b86ea57655d
blerineberisha <69892200+blerineberisha@users.noreply.github.com>
2021-09-29 09:39:46 +0200

    BookService optimised

---
+++ shop/src/BookService.tsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export class BookService{
+    
+    private client = axios.create({baseURL: 'http://localhost:3001/books'});
+    getAllBooks(){
+        return this.client.get("")
+    };
+
+    getBookByID(id: string){
+        return this.client.get("" + id)
+    };
+}
