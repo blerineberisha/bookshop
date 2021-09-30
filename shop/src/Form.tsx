14a6d6ad182424feb46b47af30b15a3f59e39ff2
blerineberisha <69892200+blerineberisha@users.noreply.github.com>
2021-09-30 10:59:09 +0200

    replaced validation functions with Yup validation

--- shop/src/Form.tsx
+++ shop/src/Form.tsx
@@ -3,13 +3,12 @@ import * as React from "react";
 import { Formik, Field, Form, FormikHelpers } from "formik";
 import * as Yup from "yup";
 import "./Form.css";
-
 interface Values {
   firstName: string;
   lastName: string;
   email: string;
+  password: string;
 }
-
 const FormElement = () => {
   const SignupSchema = Yup.object().shape({
     firstName: Yup.string()
@@ -28,25 +27,6 @@ const FormElement = () => {
       .max(50, "Too long!")
       .required("Please enter a password."),
   });
-  /*function validateEmail(value: string) {
-    let error;
-    if (!value) {
-      error = "Email address is required";
-      alert(error);
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-      error = "Invalid email address";
-      alert(error);
-    }
-    return error;
-  }
-
-  function validateName(value: string) {
-    let error;
-    if (!value) {
-      error = "Name is required";
-      alert(error);
-    }
-  }*/
 
   return (
     <div>
@@ -55,6 +35,7 @@ const FormElement = () => {
           firstName: "",
           lastName: "",
           email: "",
+          password: "",
         }}
         onSubmit={(
           values: Values,
@@ -69,70 +50,77 @@ const FormElement = () => {
         validateOnBlur={false}
         validateOnChange={false}
       >
-        <div>
-          <Form>
-            <fieldset>
-              <legend>Sign up</legend>
-              <div>
-                <label htmlFor="firstName">First Name</label>
-              </div>
-              <div>
-                <Field
-                  className="field"
-                  id="firstName"
-                  name="firstName"
-                  placeholder="John"
-                  required
-                  // validate={validateName}
-                />
-              </div>
-              <div>
-                <label htmlFor="lastName">Last Name</label>
-              </div>
-              <div>
-                <Field
-                  className="field"
-                  id="lastName"
-                  name="lastName"
-                  placeholder="Doe"
-                  required
-                  //validate={validateName}
-                />
-              </div>
-              <div>
-                <label htmlFor="email">Email</label>{" "}
-              </div>
-              <div>
-                <Field
-                  className="field"
-                  id="email"
-                  name="email"
-                  placeholder="john@doe.com"
-                  required
-                  type="email"
-                  //validate={validateEmail}
-                />
-              </div>
-              <div>
-                <label htmlFor="password">Password</label>{" "}
-              </div>
-              <div>
-                <Field
-                  className="field"
-                  id="password"
-                  name="password"
-                  placeholder="········"
-                  required
-                  type="password"
-                  //validate={validateEmail}
-                />
-              </div>
-              <div className="button">
-                <button type="submit" className="sumbit">Submit</button>
-              </div>
-            </fieldset>
-          </Form>
-        </div>
+        {({ errors, touched }) => (
+          <div>
+            <Form>
+              <fieldset>
+                <legend>Sign up</legend>
+                <div>
+                  <label htmlFor="firstName">First Name</label>
+                </div>
+                <div>
+                  <Field
+                    className="field"
+                    id="firstName"
+                    name="firstName"
+                    placeholder="John"
+                  />
+                  {errors.firstName && touched.firstName && (
+                    <div className="error">{errors.firstName}</div>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="lastName">Last Name</label>
+                </div>
+                <div>
+                  <Field
+                    className="field"
+                    id="lastName"
+                    name="lastName"
+                    placeholder="Doe"
+                  />
+                  {errors.lastName && touched.lastName && (
+                    <div className="error">{errors.lastName}</div>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="email">Email</label>{" "}
+                </div>
+                <div>
+                  <Field
+                    className="field"
+                    id="email"
+                    name="email"
+                    placeholder="john@doe.com"
+                  />
+                  {errors.email && touched.email && (
+                    <div className="error">{errors.email}</div>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="password">Password</label>{" "}
+                </div>
+                <div>
+                  <Field
+                    className="field"
+                    id="password"
+                    name="password"
+                    placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;"
+                    type="password"
+                  />
+                  {errors.password && touched.password && (
+                    <div className="error">{errors.password}</div>
+                  )}
+                </div>
+                <div className="button-submit">
+                  <button type="submit" className="sumbit">
+                    Submit
+                  </button>
+                </div>
+              </fieldset>
+            </Form>
+          </div>
+        )}
       </Formik>
     </div>
   );
